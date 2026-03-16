74ed4c5322d8ba5a286bff1fb85fd32cf8ed40c9
keyloor <crkeylor23@gmail.com>
2026-03-16 11:43:54 -0600

    Data Structures 1

---
+++ Clases/3-Estrucutras/1/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection <String> vehiculos = new ArrayList<String>();
+        
+        vehiculos.add("Bicicleta");
+        vehiculos.add("Carro");
+        vehiculos.add("Avión");
+        
+        vehiculos.forEach(vehiculo -> System.out.println(vehiculo));
+    }
+}

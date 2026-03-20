95bd5eb8184c89ae12ed68ba80839ddc5dade605
keyloor <crkeylor23@gmail.com>
2026-03-20 11:33:11 -0600

    More examples

--- Clases/3-Estructuras/2/Main.java
+++ Clases/3-Estructuras/2/Main.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +19,16 @@ public class Main {
         System.out.println("equals      > " + lista1.equals(lista2));
         System.out.println("remove      > " + lista2.remove(928));
         System.out.println("removeAll   > " + lista1.removeAll(lista2));
+
+        List<Integer> lista3 = new ArrayList<>();
+        lista3.addAll(lista2);
+        System.out.println("Get         > " + lista3.get(0));
+        System.out.println("Set         > " + lista3.set(0, 2));
+        System.out.println("IndexOf     > " + lista3.indexOf(98));
+        System.out.println("IndexOf     > " + lista3.indexOf(123));
+        System.out.println("Remove      > " + lista3.remove(0));
+
+        
         
     }
     

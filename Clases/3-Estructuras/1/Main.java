b85533237ff1a9c94497a4c73cf7ce01db693dc1
keyloor <crkeylor23@gmail.com>
2026-03-27 08:03:54 -0600

    new

--- Clases/3-Estructuras/1/Main.java
+++ Clases/3-Estructuras/1/Main.java
@@ -1,10 +1,13 @@
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Collection <String> vehiculos = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        list.add("DSNJALDNJ");
         
         vehiculos.add("Bicicleta");
         vehiculos.add("Carro");
